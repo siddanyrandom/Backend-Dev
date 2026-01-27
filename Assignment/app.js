ac62003eebc676c27588022167057d4cc2896cb0
siddanyrandom <siddharthasaxena11a@gmail.com>
2026-01-27 11:49:52 +0530

    Create app.js

---
+++ Assignment/app.js
@@ -0,0 +1,7 @@
+const utils = require('./stringUtils');
+
+let text = "hello guys";
+
+console.log("Capitalized:", utils.capitalize(text));
+console.log("Reversed:", utils.reverse(text));
+console.log("Vowel Count:", utils.countVowels(text));
