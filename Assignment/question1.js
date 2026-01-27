8565f8a8365ebbfe8e32b24c12b40e6ccb242916
siddanyrandom <siddharthasaxena11a@gmail.com>
2026-01-27 14:50:09 +0530

    Create question1.js to explain file operations

---
+++ Assignment/question1.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const ans = `Difference between synchronous and asynchronous file operations:
+
+Synchronous file operations block the execution of the program until
+the operation is completed.
+
+Asynchronous file operations do not block execution and use callbacks
+to handle the result after completion.
+`;
+
+fs.writeFile("q1_ans.txt", ans, "utf8", ((err)=>{
+    if(err){
+        console.log(err);
+    } else {
+        console.log("File created with proper answer");
+    }
+}));
