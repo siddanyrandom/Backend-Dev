209f3d2584fb3536972d90348a1ed8d5c1208f79
siddanyrandom <siddharthasaxena11a@gmail.com>
2026-01-27 15:11:55 +0530

    Update question2.js

--- Assignment/question2.js
+++ Assignment/question2.js
@@ -6,7 +6,7 @@ File streams should be used when dealing with large files so that data
 is processed in chunks, reducing memory usage.
 `;
 
-fs.writeFile("q2_ans.txt", ans, "utf8", ((err)=>{
+fs.writeFile("q2.txt", ans, "utf8", ((err)=>{
     if(err){
         console.log(err);
     } else {
